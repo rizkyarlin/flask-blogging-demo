29ac1f1866422b285ffe527d0dc10101dfba6529
Rizky Arlin <rizkyarlin@Rizkys-MacBook-Air.local>
2023-12-27 19:14:30 +0800

    update config

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
